d89f156ac2afbbb1371dbca093025a2e466f5117
MaoTou <maopucheng@gmail.com>
2023-05-25 22:47:59 +0800

    Update sidebar.tsx
    mt

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,7 +119,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>MaoTou's ChatGPT</div>
+        <div className={styles["sidebar-title"]}>MaoTou&apos;s ChatGPT</div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
         </div>
